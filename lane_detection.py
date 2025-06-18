fe61be59b118668899f377abc12547cab287f04f
Siyan-G <gsy7418@gmail.com>
2025-06-18 15:24:51 +0800

    update code to match cuda

--- lane_detection.py
+++ lane_detection.py
@@ -35,10 +35,18 @@ def pred2coords(pred, row_anchor, col_anchor, local_width=1, original_image_widt
     batch_size, num_grid_row, num_cls_row, num_lane_row = pred['loc_row'].shape
     _, num_grid_col, num_cls_col, num_lane_col = pred['loc_col'].shape
 
-    max_indices_row = pred['loc_row'].argmax(1).to(pred['loc_row'].device)
-    valid_row = pred['exist_row'].argmax(1).to(pred['exist_row'].device)
-    max_indices_col = pred['loc_col'].argmax(1).to(pred['loc_col'].device)
-    valid_col = pred['exist_col'].argmax(1).to(pred['exist_col'].device)
+    max_indices_row = pred['loc_row'].argmax(1).cpu()
+    # n , num_cls, num_lanes
+    valid_row = pred['exist_row'].argmax(1).cpu()
+    # n, num_cls, num_lanes
+
+    max_indices_col = pred['loc_col'].argmax(1).cpu()
+    # n , num_cls, num_lanes
+    valid_col = pred['exist_col'].argmax(1).cpu()
+    # n, num_cls, num_lanes
+
+    pred['loc_row'] = pred['loc_row'].cpu()
+    pred['loc_col'] = pred['loc_col'].cpu()
 
     coords = []
     row_lane_idx = [1, 2]
@@ -49,11 +57,8 @@ def pred2coords(pred, row_anchor, col_anchor, local_width=1, original_image_widt
         if valid_row[0,:,i].sum() > num_cls_row / 2:
             for k in range(valid_row.shape[1]):
                 if valid_row[0,k,i]:
-                    all_ind = torch.arange(
-                        max(0, max_indices_row[0,k,i] - local_width),
-                        min(num_grid_row - 1, max_indices_row[0,k,i] + local_width) + 1,
-                        device=max_indices_row.device
-                    )
+                    all_ind = torch.tensor(list(range(max(0,max_indices_row[0,k,i] - local_width), min(num_grid_row-1, max_indices_row[0,k,i] + local_width) + 1)))
+                    
                     out_tmp = (pred['loc_row'][0,all_ind,k,i].softmax(0) * all_ind.float()).sum() + 0.5
                     out_tmp = out_tmp / (num_grid_row-1) * original_image_width
                     tmp.append((int(out_tmp), int(row_anchor[k] * original_image_height)))
@@ -64,12 +69,10 @@ def pred2coords(pred, row_anchor, col_anchor, local_width=1, original_image_widt
         if valid_col[0,:,i].sum() > num_cls_col / 4:
             for k in range(valid_col.shape[1]):
                 if valid_col[0,k,i]:
-                    all_ind = torch.arange(
-                        max(0, max_indices_col[0,k,i] - local_width),
-                        min(num_grid_col - 1, max_indices_col[0,k,i] + local_width) + 1,
-                        device=max_indices_col.device
-                    )
+                    all_ind = torch.tensor(list(range(max(0,max_indices_col[0,k,i] - local_width), min(num_grid_col-1, max_indices_col[0,k,i] + local_width) + 1)))
+                    
                     out_tmp = (pred['loc_col'][0,all_ind,k,i].softmax(0) * all_ind.float()).sum() + 0.5
+
                     out_tmp = out_tmp / (num_grid_col-1) * original_image_height
                     tmp.append((int(col_anchor[k] * original_image_width), int(out_tmp)))
             coords.append(tmp)
@@ -147,7 +150,7 @@ if __name__ == "__main__":
                 pred = net(imgs)
 
             vis = cv2.imread(os.path.join(cfg.data_root, names[0]))
-            coords = pred2coords(pred, cfg.row_anchor, cfg.col_anchor, original_image_width=img_w, original_image_height=img_h)
+            coords = pred2coords(pred, cfg.row_anchor, cfg.col_anchor, original_image_width=img_w, original_image_height=img_h, device=device)
 
             # Timestamp extraction
             basename = os.path.basename(names[0])
